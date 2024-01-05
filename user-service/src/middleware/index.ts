8cf29bda7df5e4582cfd7ecf9b76236eda51061e
Troy Andrada <christiantroyandrada@gmail.com>
2024-01-05 21:43:43 +0800

    init: scaffold user-service with auth, MongoDB + Docker

---
+++ user-service/src/middleware/index.ts
@@ -0,0 +1,50 @@
+import { ErrorRequestHandler } from 'express'
+import { APIError } from '../utils'
+
+export const errorMiddleware: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next,
+) => {
+  let error = err
+
+  if (!(error instanceof APIError)) {
+    const statusCode = error.statusCode || (error instanceof Error ? 400 : 500)
+    const message = error.message || (
+      statusCode === 500 ? 'Internal Server Error' : 'Bad Request'
+    )
+    error = new APIError(statusCode, message, false, err.stack.toString())
+  }
+  next(error)
+}
+
+export const errorHandler: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next,
+) =>{
+  let { statusCode, message } = err
+  const environment = process.env.NODE_ENV ?? 'development'
+
+  if (environment === 'production' && !err.isOperational) {
+    statusCode = 500
+    message = 'Internal Server Error'
+  }
+
+  res.locals.errorMessage = err.message
+
+  const response = {
+    code: statusCode,
+    message,
+    ...(environment === 'development' && { stack: err.stack }),
+  }
+
+  if (environment === 'development') {
+    console.error(err)
+  }
+
+  res.status(statusCode).json(response)
+  next()
+}
---
+++ user-service/src/server.ts
@@ -0,0 +1,50 @@
+import express, { Express } from 'express'
+import { Server } from 'http'
+import userServiceRouter from './routes/userServiceRouter'
+import { errorMiddleware, errorHandler } from './middleware'
+import { connectDB } from './database'
+import config from './config/config'
+import { rabbitMQService } from './services/RabbitMQService'
+
+const app: Express = express()
+let server: Server
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(userServiceRouter)
+app.use(errorMiddleware)
+app.use(errorHandler)
+
+connectDB()
+
+server = app.listen(config.PORT, () => {
+  console.log(`Server is running on port ${config.PORT}`)
+})
+
+const initRabbitMQClient = async () => {
+  try {
+    await rabbitMQService.connect()
+    console.log('RabbitMQ client initialized. now listening...')
+  } catch (e) {
+    console.error(`Failed to initialize RabbitMQ client: ${e}`)
+  }
+}
+initRabbitMQClient()
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      console.info('server closed')
+      process.exit(1)
+    })
+  } else {
+    process.exit(1)
+  }
+}
+
+const unexpectedErrorHandler = (error: unknown) => {
+  console.error(error)
+  exitHandler()
+}
+
+process.on('uncaughtException', unexpectedErrorHandler)
+process.on('unhandledRejection', unexpectedErrorHandler)
