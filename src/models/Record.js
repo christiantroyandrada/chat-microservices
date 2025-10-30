b500f776fa720825360a68b8bbca455949b94eb2
Christian Troy Andrada <christian.adrada@collabera.com>
2025-10-31 02:45:19 +0800

    remove existing files to be replaced with new microservice empowerment code

--- src/models/Record.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const schema = new Schema({
-  title: {type: String, required: true},
-  completed: Boolean,
-});
-
-module.exports = mongoose.model('Record', schema);
